b3024a44999c9e2c3a7d893fff68968db5f10276
Arash Tadjiki <atadjiki@gmail.com>
2019-01-17 22:37:17 -0700

    Testing camera tracking

---
+++ synthfm/Assets/SetPiece.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+public class SetPiece : MonoBehaviour
+{
+
+    public Cinemachine.CinemachineVirtualCamera mainCamera;
+    public Cinemachine.CinemachineVirtualCamera setCamera;
+
+    public TurntableController player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    { 
+
+        if (collision.gameObject == player.gameObject)
+        {
+            Debug.Log("On Trigger Enter");
+            mainCamera.enabled = false;
+            setCamera.enabled = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+       
+        if (collision.gameObject == player.gameObject)
+        {
+            Debug.Log("On Trigger Close");
+            mainCamera.enabled = true;
+            setCamera.enabled = false;
+
+        }
+    }
+
+}
