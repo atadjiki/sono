caf5a4b8dcbe40f78d2886dd02e46a027248ce5b
Arash Tadjiki <atadjiki@gmail.com>
2019-01-22 01:14:12 -0700

    On-screen nav pointer
    points in the direction of a target

---
+++ synthfm/Assets/Scripts/Navpoint.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Navpoint : MonoBehaviour
+{
+
+    public GameObject sphere;
+    public GameObject target;
+    public GameObject pointer;
+    public float minDistance = 200;
+    private Vector3 center;
+    public float radius = 50;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        sphere.GetComponent<SphereCollider>().radius = radius;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(Vector3.Distance(sphere.transform.position, target.transform.position) > minDistance)
+        {
+            pointer.gameObject.SetActive(true);
+            pointer.transform.position = sphere.GetComponent<SphereCollider>().ClosestPointOnBounds(target.transform.position);
+        }
+        else
+        {
+            pointer.gameObject.SetActive(false);
+        }
+
+ 
+
+    }
+}
