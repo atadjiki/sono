68f1e72effb9649b1f93fbf84e09df0cb5aeefb8
Arash Tadjiki <atadjiki@yahoo.com>
2019-01-23 20:31:34 -0700

    Added lock title

---
+++ synthfm/Assets/Scripts/LevelManagement/LockTitle.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockTitle : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float lockTime = 10.0f;
+    public TurntableController player;
+
+    void Start()
+    {
+        StartCoroutine(Lock());
+    }
+
+    IEnumerator Lock()
+    {
+        Debug.Log(Time.time);
+        player.enabled = false;
+        yield return new WaitForSecondsRealtime(lockTime);
+        player.enabled = true;
+        Debug.Log(Time.time);
+    }
+}
