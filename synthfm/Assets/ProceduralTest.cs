69fe8bc837668bd9dbd3b1d9b112c5b7ef92a56f
Pinak Jalan <pinak.jalan@me.com>
2019-02-02 17:44:22 -0700

    Added basic framework for procedural generation

---
+++ synthfm/Assets/ProceduralTest.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProceduralTest : MonoBehaviour
+{
+    [SerializeField] private Camera mainCamera;
+    [SerializeField] private GameObject playerPos;
+
+    private GameObject left;
+    private GameObject right;
+    private GameObject above;
+    private GameObject below;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(Input.GetKeyDown(KeyCode.P)){
+
+            TakeSnapshot();
+        }
+
+    }
+
+    void TakeSnapshot()
+    {
+        //get the screen dimensions for the player
+        Vector3 screenPos = mainCamera.WorldToScreenPoint(playerPos.transform.TransformPoint(playerPos.transform.position));
+
+        left = new GameObject();
+        left.transform.position = new Vector2(screenPos.x - (Screen.width*1.1f), screenPos.y - (Screen.height/2));
+        left.AddComponent<BoxCollider2D>();
+        left.GetComponent<BoxCollider2D>().size = new Vector2(Screen.width, Screen.height);
+        left.GetComponent<BoxCollider2D>().isTrigger = true;
+        left.tag = "Left";
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        DespawnObjects(collision.gameObject.tag);
+    }
+
+    private void DespawnObjects(string dir)
+    {
+        if(dir == "Up")
+        {
+
+        }
+        else if(dir == "Bottom")
+        {
+
+        }
+        else if(dir == "Right")
+        {
+
+        }
+        else if(dir == "Left")
+        {
+
+        }
+    }
+}
