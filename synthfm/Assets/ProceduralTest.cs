383c1272335b662efe023bfb95408e1b432afd42
Pinak Jalan <pinak.jalan@me.com>
2019-02-02 18:23:09 -0700

    Added more stuff

--- synthfm/Assets/ProceduralTest.cs
+++ synthfm/Assets/ProceduralTest.cs
@@ -6,8 +6,10 @@ public class ProceduralTest : MonoBehaviour
 {
     [SerializeField] private Camera mainCamera;
     [SerializeField] private GameObject playerPos;
+    [SerializeField] private List<GameObject> puzzles;
 
     private GameObject left;
+    private GameObject leftPuzzle;
     private GameObject right;
     private GameObject above;
     private GameObject below;
@@ -25,6 +27,7 @@ public class ProceduralTest : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.P)){
 
             TakeSnapshot();
+            SpawnPuzzles();
         }
 
     }
@@ -35,11 +38,32 @@ public class ProceduralTest : MonoBehaviour
         Vector3 screenPos = mainCamera.WorldToScreenPoint(playerPos.transform.TransformPoint(playerPos.transform.position));
 
         left = new GameObject();
+        left.name = "Left";
         left.transform.position = new Vector2(screenPos.x - (Screen.width*1.1f), screenPos.y - (Screen.height/2));
         left.AddComponent<BoxCollider2D>();
         left.GetComponent<BoxCollider2D>().size = new Vector2(Screen.width, Screen.height);
         left.GetComponent<BoxCollider2D>().isTrigger = true;
         left.tag = "Left";
+    }
+
+    private void SpawnPuzzles()
+    {
+       
+        int puzzleIndex = Random.Range(0, puzzles.Count - 1);
+
+        GameObject leftTransform = new GameObject();
+        leftTransform.transform.position = new Vector2(Random.Range(left.transform.position.x, left.transform.position.x + Screen.width/2), 
+            leftTransform.transform.position.y); 
+
+
+        leftPuzzle = Instantiate(puzzles[puzzleIndex], leftTransform.GetComponent<Transform>());
+        leftPuzzle.AddComponent<Cinemachine.CinemachineVirtualCamera>();
+        leftPuzzle.GetComponent<SetPiece>().setPieceCamera = leftPuzzle.GetComponent<Cinemachine.CinemachineVirtualCamera>();
+        leftPuzzle.GetComponent<SetPiece>().setPieceCamera.Follow = leftPuzzle.transform;
+        
+        leftPuzzle.name = "Left Puzzzle";
+        
+        Debug.Log("Left puzzle at " + leftPuzzle.transform.position.ToString());
 
     }
 
