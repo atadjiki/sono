5b15d5154a346b75896fd6f57fa4d61a2f5ddcb4
Rohan <rohanpatel1899@gmail.com>
2019-02-07 15:04:23 -0700

    Adding ColorMaze: Added colorable plates

---
+++ synthfm/Assets/Scripts/LevelManagement/PuzzleComponents/ColorMaze/ColorManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorManager : MonoBehaviour
+{
+  
+    [Header("Colors for the brick")]
+    public Color baseColor;
+    public Color errorColor;
+    public Color activeColor;
+
+    public int sequenceNo;
+
+    private SpriteRenderer _renderer;
+    private Color currentColor;
+    // add a couroutine to change from error to default
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _renderer = this.GetComponent<SpriteRenderer>();
+        _renderer.color = baseColor;
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (LevelManager.instance.getPlayer() == collision.gameObject)
+        {
+           
+            // check puzzle state and update the color if true sequence
+            // if(this.sequenceNo == puzle.getCurrentSequenceNo(); )
+                _renderer.color = activeColor;
+            // else error color
+
+                // Start coroutine to change back to normal color
+
+        }
+    }
+
+    // Set
+    public void setCurrentColor()
+    {
+
+    }
+
+    // Set
+    public void setBaseColor(Color i_color)
+    {
+        baseColor = i_color;
+    }
+}
---
+++ synthfm/Assets/Scripts/LevelManagement/PuzzleComponents/ColorMaze/changeIt.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changeIt : MonoBehaviour
+{
+    private SpriteRenderer rendererThis;
+    private Color defaultColor;
+
+    private void Start()
+    {
+        rendererThis = this.GetComponent<SpriteRenderer>();
+        defaultColor = rendererThis.color;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        rendererThis.color = Color.green;
+        StartCoroutine(setBack());
+    }
+
+    IEnumerator setBack()
+    {
+        yield return new WaitForSeconds(5.0f);
+        rendererThis.color = defaultColor;
+    }
+
+}
