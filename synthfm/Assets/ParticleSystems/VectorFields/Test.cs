780233f39b9f24991a751d1fc9205a1fc539e552
Travis Treadway <Travis.treadway55@gmail.com>
2019-02-07 18:02:38 -0700

    Particle system Jazz

---
+++ synthfm/Assets/ParticleSystems/VectorFields/Test.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.VFX;
+
+public class Test : MonoBehaviour
+{
+    public VisualEffect VE;
+    public Texture3D tex;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            VE.SetTexture("Vectorfields", tex);
+        }
+    }
+}
+ 
