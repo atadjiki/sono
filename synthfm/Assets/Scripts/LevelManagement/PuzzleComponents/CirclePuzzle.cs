26ed629d73488654a9a28d5e87cb2eae846db89f
Arash Tadjiki <atadjiki@gmail.com>
2019-02-09 20:47:10 -0700

    Kind of working loop detection

--- synthfm/Assets/Scripts/LevelManagement/PuzzleComponents/CirclePuzzle.cs
+++ synthfm/Assets/Scripts/LevelManagement/PuzzleComponents/CirclePuzzle.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 /*
  * Boilerplate for a new puzzle 
- */ 
+ */
 public class CirclePuzzle : Puzzle
 {
 
@@ -18,6 +18,11 @@ public class CirclePuzzle : Puzzle
 
     public GameObject forceField;
 
+    public GameObject[] targets;
+
+    float threshhold = 1f;
+    Vector3[] positions = new Vector3[1024];
+
     // Start is called before the first frame update
     void Awake()
     {
@@ -27,14 +32,20 @@ public class CirclePuzzle : Puzzle
     // Update is called once per frame
     void Update()
     {
-        if (!complete && playerEntered) 
+        if (!complete && playerEntered)
         {
             if (currentFrames == maxFrames)
             {
-                if (CheckForClosedLoop())
+                Debug.Log("Checking for loop");
+                bool DoesContain = false;
+                if (CheckForClosedLoop(ref DoesContain))
                 {
-                    complete = true;
-                    ReleaseCage();
+                    if (DoesContain)
+                    {
+                        complete = true;
+                        ReleaseCage();
+                    }
+
                 }
                 currentFrames = 0;
             }
@@ -47,26 +58,42 @@ public class CirclePuzzle : Puzzle
 
     }
 
-    bool CheckForClosedLoop()
+    bool CheckForClosedLoop(ref bool DoesContain)
     {
-        int lookBehind = 1024 * 1024;
-        Vector3[] positions = new Vector3[lookBehind];
-        int result = playerTrail.GetPositions(positions);
 
-        foreach(Vector3 position in positions)
+        Debug.Log("Found " + playerTrail.positionCount + " vertices in trail");
+        positions = new Vector3[playerTrail.positionCount];
+        int result = playerTrail.GetPositions(positions);
+        bool closed = false;
+        Vector3 intersection = new Vector3();
+
+
+        foreach (Vector3 position in positions)
         {
             float xDist = Mathf.Abs(position.x - playerTransform.position.x);
             float yDist = Mathf.Abs(position.y - playerTransform.position.y);
 
-            if(xDist > 0 && xDist < 1 && yDist > 0 && yDist < 1)
+            if (xDist < threshhold && yDist < threshhold)
             {
                 Debug.Log("Loop closed!");
-                return true;
+                Debug.Log(position.ToString() + ", collided with " + playerTransform.position.ToString());
+                closed = true;
+                intersection = position;
+                break;
             }
 
         }
 
-        return false;
+        if (closed)
+        {
+            DoesContain = CheckObjectsInside(intersection);
+            return closed;
+        }
+        else
+        {
+            return false;
+        }
+
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -85,6 +112,12 @@ public class CirclePuzzle : Puzzle
 
     void ReleaseCage()
     {
+
+        foreach(GameObject target in targets)
+        {
+            Destroy(target);
+        }
+
         //lower the force field and turn off its noise
         forceField.GetComponent<PointEffector2D>().enabled = false;
         forceField.GetComponent<AudioSource>().enabled = false;
@@ -94,6 +127,38 @@ public class CirclePuzzle : Puzzle
         {
             particle.Stop(); //Stop the animations instead of destroying them for the dissipation effect 
         }
+    }
+
+    bool CheckObjectsInside(Vector3 intersection)
+    {
+
+        List<Vector3> positionsToCheck = new List<Vector3>(positions);
+        int startIndex = positionsToCheck.IndexOf(intersection);
+        positionsToCheck = positionsToCheck.GetRange(startIndex, positionsToCheck.Count - startIndex);
+
+        foreach(GameObject target in targets)
+        {
+            if(!ContainsPoint(positionsToCheck.ToArray(), target.transform.position))
+            {
+                return false;
+            }
+        }
+
+        Debug.Log("Loop contains targets!");
+        return true;
+    }
+
+    static bool ContainsPoint(Vector3[] polyPoints, Vector3 p)
+    {
+        int j = polyPoints.Length - 1;
+        bool inside = false;
+        for (int i = 0; i < polyPoints.Length; j = i++)
+        {
+            if (((polyPoints[i].y <= p.y && p.y < polyPoints[j].y) || (polyPoints[j].y <= p.y && p.y < polyPoints[i].y)) &&
+               (p.x < (polyPoints[j].x - polyPoints[i].x) * (p.y - polyPoints[i].y) / (polyPoints[j].y - polyPoints[i].y) + polyPoints[i].x))
+                inside = !inside;
+        }
+        return inside;
     }
 
 }
