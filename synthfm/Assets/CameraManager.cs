20a483191dc2e954a7e59e67fe08b14fd53cf183
Pinak Jalan <pinak.jalan@me.com>
2019-02-10 18:55:09 -0700

    More progress on hub project

---
+++ synthfm/Assets/CameraManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    public GameObject m_Main;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Main.GetComponent<Cinemachine.CinemachineVirtualCamera>().Follow = GameObject.FindGameObjectWithTag("Player").transform;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
