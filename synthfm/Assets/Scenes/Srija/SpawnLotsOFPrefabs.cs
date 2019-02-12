17b14e1448c696028d2cd8df6222cfc578824fd0
Srija <u1209446@utah.edu>
2019-02-12 02:54:31 -0700

    Sand? Noisy sand.

---
+++ synthfm/Assets/Scenes/Srija/SpawnLotsOFPrefabs.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnLotsOFPrefabs : MonoBehaviour //yeah... that's a typo in the class name. I'm too lazy to fix it.
+{
+    public GameObject ObjectToSpawn;
+    public int NumberOfObjectsToSpawn = 50;
+    public float SpawnRadius = 10.0f;
+
+    //There's probably a better way to do this and I'm just making life difficult.
+    void Awake()
+    {
+        // Basically defines a sphere (or in our case, a circle) and then picks a random point in it and spawns the prefab there.
+        // Yes I totally stole and modified the SpawnPrefab script, because that one spawns over time.
+        for (int i = 0; i < NumberOfObjectsToSpawn; i++)
+        {
+            Vector3 offset = Random.insideUnitSphere;
+            offset.z = 0f;
+
+            Instantiate(ObjectToSpawn, transform.position + offset * SpawnRadius, Quaternion.identity);
+        }
+    }
+}
