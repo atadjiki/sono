b19f2620d3389524194caeb555ae3df4e17c5eb7
Travis Treadway <Travis.treadway55@gmail.com>
2019-02-14 09:12:17 -0700

    Added Particles

---
+++ synthfm/Assets/ParticleSystems/NewBehaviourScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public GameObject particle;
+    public List<ParticleCollisionEvent> collisionEvents;
+    public Material newMaterial;
+    // Start is called before the first frame update
+    void Start()
+    {
+        collisionEvents = new List<ParticleCollisionEvent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnParticleCollision(GameObject other)
+    {
+        if (other.transform.tag == "Player")
+        {
+            System.Console.WriteLine("heekjfaslk;");
+        }
+    }
+}
