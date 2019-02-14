bf6a5bd09954abd3fff4dc76207a9165563ce928
Rohan <rohanpatel1899@gmail.com>
2019-02-14 11:45:38 -0700

    Work Here Form Now: Restoring stuff

---
+++ synthfm/Assets/Scripts/LevelManagement/PuzzleComponents/ColorMaze/Notifier.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Notifier : MonoBehaviour
+{
+
+    public int seqNo;
+    public PuzzleBehavior puzzleBehavior;
+    public PuzzleBehavior.State _curState;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _curState = PuzzleBehavior.State.OFF;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+      //  if (!(_curState == PuzzleBehavior.State.ON))
+        {
+            puzzleBehavior.notify(this);
+        }
+    }
+
+    public void changeState(PuzzleBehavior.State i_state) { _curState = i_state; }
+
+    public PuzzleBehavior.State getState() { return _curState; }
+
+}
--- synthfm/Assets/Scripts/LevelManagement/PuzzleComponents/ColorMaze/PuzzleBehavior.cs
+++ synthfm/Assets/Scripts/LevelManagement/PuzzleComponents/ColorMaze/PuzzleBehavior.cs
@@ -4,15 +4,112 @@ using UnityEngine;
 
 public class PuzzleBehavior : MonoBehaviour
 {
+
+    public enum State { OFF, ON, Error };
+
+    [Header("Change this to private later !!!")]
+    // TODO :: chnage to private once done with debuging
+    public ColorManager[] colorPlates;
+
+    public int currentSequence = 0;
+    public bool IsComplete;
+
+    [Header("Audio Source")]
+      public AudioSource PuzzleaudioSource;
+
+    [Header("AudioTones")]
+    public AudioClip[] pianoKeys;
+    public AudioClip errorTone;
+
+    // shaking effect
+    //private Transform transform;
+    //private float shakeDuration;
+    //private float shakeMagnitude = 0.8f;
+    //private float dampingSpeed = 1.0f;
+    //Vector3 initialPosition;
+
+
+    private void Awake()
+    {
+        //if (transform == null)
+        //{
+        //    transform = GetComponent(typeof(Transform)) as Transform;
+        //}
+    }
+
     // Start is called before the first frame update
     void Start()
     {
+        IsComplete = false;
+        PuzzleaudioSource = this.GetComponent<AudioSource>();
         
+        //audioSource.clip = pianoKeys[0];
+        Debug.Log(transform.Find("ColorPlates").childCount);
+        //for(int i=0; i < transform.Find("ColorPlates").childCount; i++)
+        {
+            //   ColorManager x = transform.Find("ColorPlates").GetChild(1).GetComponent<ColorManager>();
+        }
+
+      //  transform = this.GetComponent<Transform>();
+        //initialPosition = transform.localPosition;
+        //shakeDuration = 0;
     }
 
-    // Update is called once per frame
-    void Update()
+    // Update
+    private void Update()
     {
-        
+       // shakeDuration = 2;
+        //if (shakeDuration > 0)
+        //{
+        //    transform.localPosition = initialPosition + Random.insideUnitSphere * shakeMagnitude;
+
+        //    shakeDuration -= Time.deltaTime * dampingSpeed;
+        //}
+        //else
+        //{
+        //    shakeDuration = 0f;
+        //    transform.localPosition = initialPosition;
+        //}
+    }
+
+    public void notify(Notifier i_notifier)
+    {
+        if (!IsComplete)
+        {
+            // check if it matche to current sequence and change color
+            if (currentSequence == i_notifier.seqNo)
+            {
+                int toPlay = currentSequence % pianoKeys.Length;
+                PuzzleaudioSource.clip = pianoKeys[toPlay];
+                PuzzleaudioSource.Play();
+
+                colorPlates[currentSequence].changeToActive();
+                i_notifier.changeState(State.ON);
+
+                currentSequence++;
+            }
+            else // error state
+            {
+                foreach (ColorManager _cm in colorPlates)
+                {
+                    _cm.changeToFail();
+                }
+
+                // Play error tone
+                PuzzleaudioSource.clip = errorTone;
+                PuzzleaudioSource.Play();
+
+                i_notifier.changeState(State.OFF);
+
+                currentSequence = 0;
+
+              //  shakeDuration = 2f;
+            }
+        }
+        if (currentSequence == 18)
+        {
+             IsComplete = true;
+            currentSequence = 0;
+        }
     }
 }
