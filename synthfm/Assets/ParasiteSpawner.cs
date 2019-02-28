874cb1af2b7d41a77e297151054fd7b02ccb8a6c
Arash Tadjiki <atadjiki@yahoo.com>
2019-02-28 14:55:43 -0700

    Parasite spawner and parasite prefab

---
+++ synthfm/Assets/ParasiteSpawner.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParasiteSpawner : MonoBehaviour
+{
+
+    public GameObject parasitePrefab;
+    public Transform player;
+    public Camera mainCamera;
+    public int maxParasites;
+    public float secsUntilNextSpawn;
+    private List<GameObject> parasites;
+    public bool active = true;
+
+    private void Awake()
+    {
+        if (player == null)
+            player = GameObject.Find("Player").transform;
+        if (mainCamera == null)
+            mainCamera = GameObject.Find("Main Camera").GetComponent<Camera>();
+
+        parasites = new List<GameObject>();
+
+        StartCoroutine("DoSpawn");
+
+    }
+
+    Vector3 SpawnNextParasite()
+    {
+        GameObject parasite = Instantiate<GameObject>(parasitePrefab);
+        parasite.transform.parent = this.transform;
+
+
+        Vector3 spawnLocation = player.position; //get position of player
+        spawnLocation += new Vector3(Screen.width, Screen.height, 0);
+        parasite.transform.position = spawnLocation;
+
+        parasite.GetComponent<ParasiteController>().followTarget = player;
+        parasite.GetComponent<ParasiteController>().currentState = ParasiteController.states.FOLLOW;
+
+        parasites.Add(parasite);
+
+        return spawnLocation;
+    }
+
+
+    IEnumerator DoSpawn()
+    {
+        while (active)
+        {
+            if (parasites.Count < maxParasites)
+            {
+                Debug.Log("Spawning Parasite");
+                SpawnNextParasite();
+                yield return new WaitForSeconds(secsUntilNextSpawn);
+            }
+        }
+    }
+    
+    public void KillParasites()
+    {
+        List<GameObject> toDestroy = new List<GameObject>(parasites);
+        parasites.Clear();
+
+        foreach (GameObject parasite in toDestroy)
+        {
+            Destroy(parasite);
+        }
+    }
+
+
+}
---
+++ synthfm/Assets/Scripts/Fragments/ParasiteController.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParasiteController : MonoBehaviour
+{
+
+    private Transform player;
+
+    public enum states { IDLE, FLEE, FOLLOW };
+    public states currentState;
+
+    private Rigidbody2D rb;
+    public float acceleration;
+    public float maxSpeed;
+    public float turnSpeed;
+
+    public bool isAttached;
+
+    public Transform followTarget;
+
+    //public AudioSource audioSource;
+
+    private void Awake()
+    {
+        player = GameObject.Find("Player").transform;
+        rb = GetComponent<Rigidbody2D>();
+
+        if(followTarget == null)
+        {
+            followTarget = player;
+        }
+
+    }
+
+    private void Update()
+    {
+        RunState(currentState);
+    }
+
+    public void RunState(states state)
+    {
+        switch (state)
+        {
+            case states.IDLE:
+                Idle();
+                break;
+            case states.FOLLOW:
+                Follow();
+                break;
+            case states.FLEE:
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void Idle()
+    {
+        rb.velocity = Vector2.Lerp(rb.velocity, Vector2.zero, 1f);
+    }
+
+    private void Follow()
+    {
+        if (followTarget != null)
+        {
+            //get direction to add torque
+            Vector3 evadeDirection = (followTarget.position - transform.position).normalized;
+            float angle = Mathf.Atan2(evadeDirection.y, evadeDirection.x) * Mathf.Rad2Deg;
+            Quaternion rotation = Quaternion.AngleAxis(angle - 90f, Vector3.forward);
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, turnSpeed * Time.deltaTime);
+
+            //accelerate
+            float restrictedAccel = Vector2.Distance(transform.position, player.position) / 25f * acceleration;
+
+            rb.AddForce(transform.up * restrictedAccel * Time.deltaTime);
+        }
+    }
+
+    public states GetState()
+    {
+        return currentState;
+    }
+}
