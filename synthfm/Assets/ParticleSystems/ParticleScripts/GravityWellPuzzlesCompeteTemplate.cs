d09ddb92b47603aabf197a21e8ac46a53919cfa4
Arash Tadjiki <atadjiki@yahoo.com>
2019-03-10 18:33:09 -0600

    Recovered Travis's vfx

---
+++ synthfm/Assets/ParticleSystems/ParticleScripts/GravityWellPuzzlesCompeteTemplate.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.VFX;
+
+public class GravityWellPuzzlesCompeteTemplate : MonoBehaviour
+{
+    public VisualEffect GravityWell;
+    public bool PuzzleComplete;
+    public Gradient GradientOnKill;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(PuzzleComplete == true)
+        {
+            GravityWell.SetFloat("Sett0-100Onkill", 100);
+            GravityWell.SetGradient("GradientOnstart", GradientOnKill);
+            GravityWell.SetFloat("EmissionRate", 0);
+            GravityWell.SetFloat("EmissionRate2", 0);
+        }
+    }
+}
