e8e9b366cff0eb36c8694786976b8dcecaf51abf
Pinak Jalan <pinak.jalan@me.com>
2019-03-14 16:19:32 -0600

    Added stuff for spawning parasites and fiber world

---
+++ synthfm/Assets/SpawnFiberStuff.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnFiberStuff : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            GameObject.Find("Player").GetComponent<Navpoint>().enteredFiberWorld = true;
+            gameObject.GetComponent<FiberWorld>().enabled = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            GameObject.Find("Player").GetComponent<Navpoint>().enteredFiberWorld = false;
+            gameObject.GetComponent<FiberWorld>().enabled = false;
+
+        }
+    }
+}
