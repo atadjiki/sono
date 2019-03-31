f423661853892c9fd0b1199ec3c5a5e8b2de586f
PinakJalan <pinak.jalan@me.com>
2019-03-31 14:15:12 -0600

    Removed mouse input from RND branch

--- synthfm/Assets/Scripts/Turntable/TurntableController.cs
+++ synthfm/Assets/Scripts/Turntable/TurntableController.cs
@@ -131,7 +131,7 @@
             if (!MenuMode)
             {
                 DoAltInput();
-                DoMouseInput();
+                //DoMouseInput();
             }
             DoMIDIInput();
 
