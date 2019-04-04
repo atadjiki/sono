4cac8837d77bfcb66084a4142eaa96e13274dff9
TwilitWave <TwilitWave@gmail.com>
2019-04-04 11:23:54 -0600

    Added new realms to all the loading scripts

---
+++ synthfm/Assets/Scripts/LevelManagement/LatteWorld.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LatteWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject Psystem;
+    void Start()
+    {
+        Psystem.SetActive(true);
+        foreach(ParticleSystem particleSystem in Psystem.GetComponentsInChildren<ParticleSystem>())
+        {
+            particleSystem.Play();
+        }
+
+        killParasites();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void killParasites()
+    {
+        if(GameObject.FindGameObjectWithTag("PSpawner") != null)
+            GameObject.FindGameObjectWithTag("PSpawner").GetComponent<ParasiteSpawner>().KillParasites();
+    }
+
+    private void OnEnable()
+    {
+        Psystem.SetActive(true);
+        foreach (ParticleSystem particleSystem in Psystem.GetComponentsInChildren<ParticleSystem>())
+        {
+            particleSystem.Play();
+        }
+
+        killParasites();
+    }
+
+    private void OnDisable()
+    {
+        foreach (ParticleSystem particleSystem in Psystem.GetComponentsInChildren<ParticleSystem>())
+        {
+            particleSystem.Stop();
+        }
+
+    }
+}
