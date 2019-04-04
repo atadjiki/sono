82ff15182993c2c951ea8183069eabf4cdd42d57
Arash Tadjiki <atadjiki@yahoo.com>
2019-04-04 17:27:50 -0600

    Working on new hub

--- synthfm/Assets/Scripts/LevelManagement/HubController.cs
+++ synthfm/Assets/Scripts/LevelManagement/HubController.cs
@@ -4,26 +4,66 @@ using UnityEngine;
 
 public class HubController : MonoBehaviour
 {
-
-    private float z_rot = 1f;
+    public GameObject EntryPoint;
+    private float z_rot = 0.01f;
+    private GameObject player;
+    public float entryDistance = 100f;
+    private bool stopRotating = false;
+    public CircleCollider2D wallcollider;
+    public Rigidbody2D wallRigidBody;
+    public float angle_offset = 90;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.Find("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
         DoRotate();
+        CheckEntryPoint();
     }
     
     public void DoRotate()
     {
+        if (stopRotating) return;
 
-        this.transform.Rotate(0, 0, z_rot);
+        Vector3 v_diff = (player.transform.position - EntryPoint.transform.position);
+        float atan2 = Mathf.Atan2(v_diff.y, v_diff.x);
+        transform.rotation = Quaternion.Euler(0f, 0f, (atan2 * Mathf.Rad2Deg)+angle_offset);
 
+    }
+
+    public void CheckEntryPoint()
+    {
+
+        if (wallcollider.bounds.Contains(player.transform.position))
+        {
+            wallcollider.isTrigger = true;
+            Debug.Log("Player inside bounds");
+        }
+        else
+        {
+
+            if (Vector3.Distance(EntryPoint.transform.position, player.transform.position) <= entryDistance)
+            {
+
+                wallcollider.isTrigger = true;
+                stopRotating = true;
+                Debug.Log("Player near entry point");
+            }
+            else
+            {
+                wallcollider.isTrigger = false;
+                Debug.Log("Player outside and away from entry point " + Vector3.Distance(player.transform.position, EntryPoint.transform.position).ToString());
+            }
+
+        }
+
+        
+        
     }
 
 }
