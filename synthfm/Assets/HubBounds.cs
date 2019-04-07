c030ef28fa11c96b0db451a75a9cd3fae904c4fe
Arash Tadjiki <atadjiki@gmail.com>
2019-04-07 15:00:57 -0600

    Hub is marginally better but still wonk as fuck

---
+++ synthfm/Assets/HubBounds.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HubBounds : MonoBehaviour
+{
+
+    private HubController hub;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hub = GetComponentInParent<HubController>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        hub.insideBounds = true;
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        hub.insideBounds = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        hub.insideBounds = false;
+    }
+}
--- synthfm/Assets/Scripts/LevelManagement/HubController.cs
+++ synthfm/Assets/Scripts/LevelManagement/HubController.cs
@@ -7,9 +7,9 @@ public class HubController : MonoBehaviour
     public GameObject EntryPoint;
     public float z_rot = 0.5f;
     private GameObject player;
-    public float entryDistance = 100f;
-    private bool stopRotating = false;
-    public PolygonCollider2D wallcollider;
+    public bool inEntryPoint = false;
+    public bool insideBounds = false;
+    public CircleCollider2D wallcollider;
 
     // Start is called before the first frame update
     void Start()
@@ -20,43 +20,26 @@ public class HubController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        DoRotate();
-        CheckEntryPoint();
-    }
-    
-    public void DoRotate()
-    {
-        if (stopRotating) transform.Rotate(0, 0, z_rot/4); ;
-
-        transform.Rotate(0, 0, z_rot);
-
+       // DoRotate();
+        CheckBounds();
     }
 
-    public void CheckEntryPoint()
+    public void CheckBounds()
     {
 
-        if (wallcollider.bounds.Contains(player.transform.position))
+        Debug.Log(insideBounds);
+        if (!inEntryPoint && insideBounds)
         {
-            Debug.Log("Player inside bounds");
-        }
-        else
-        {
-
-            if (Vector3.Distance(EntryPoint.transform.position, player.transform.position) <= entryDistance)
-            {
-            
-                stopRotating = true;
-                Debug.Log("Player near entry point");
-            }
-            else
-            {
-                Debug.Log("Player outside and away from entry point " + Vector3.Distance(player.transform.position, EntryPoint.transform.position).ToString());
-            }
+            Debug.Log("Not in entry point and inside bounds");
 
         }
+        else if (!inEntryPoint && insideBounds == false)
+        {
+            Debug.Log("Not in entry point and outside bounds");
+            transform.Rotate(0, 0, z_rot);
+            Debug.Log("Rotating");
+        }
 
-        
-        
+
     }
-
 }
