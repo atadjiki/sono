bcf66c39e83397c11a1facf31179c8d101cbe69e
Arash Tadjiki <atadjiki@gmail.com>
2019-04-07 14:00:39 -0600

    Fixing rotation

--- synthfm/Assets/Scripts/LevelManagement/HubController.cs
+++ synthfm/Assets/Scripts/LevelManagement/HubController.cs
@@ -5,13 +5,11 @@ using UnityEngine;
 public class HubController : MonoBehaviour
 {
     public GameObject EntryPoint;
-    private float z_rot = 0.01f;
+    public float z_rot = 0.5f;
     private GameObject player;
     public float entryDistance = 100f;
     private bool stopRotating = false;
     public PolygonCollider2D wallcollider;
-    public Rigidbody2D wallRigidBody;
-    public float angle_offset = 90;
 
     // Start is called before the first frame update
     void Start()
@@ -28,11 +26,9 @@ public class HubController : MonoBehaviour
     
     public void DoRotate()
     {
-        if (stopRotating) return;
+        if (stopRotating) transform.Rotate(0, 0, z_rot/4); ;
 
-        Vector3 v_diff = (player.transform.position - EntryPoint.transform.position);
-        float atan2 = Mathf.Atan2(v_diff.y, v_diff.x);
-        transform.rotation = Quaternion.Euler(0f, 0f, (atan2 * Mathf.Rad2Deg)+angle_offset);
+        transform.Rotate(0, 0, z_rot);
 
     }
 
