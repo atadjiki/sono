4dde88bb38f0d7bae9d26efd94c872e59678d4cb
Srija <u1209446@utah.edu>
2019-04-16 00:42:21 -0600

    Latte Trail script initial
    We still got work to do

---
+++ synthfm/Assets/Scripts/LevelEditor/LightUpTrailEditor.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+#if UNITY_EDITOR
+[CustomEditor(typeof(LightUpTrail))]
+public class LightUpTrailEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        LightUpTrail myScript = (LightUpTrail)target;
+        if (GUILayout.Button("Build Trail"))
+        {
+            myScript.Setup();
+        }
+    }
+}
+#endif
---
+++ synthfm/Assets/Scripts/LightUpTrail.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(Collider2D))]
+public class LightUpTrail : MonoBehaviour
+{
+    Grid grid;
+    Collider2D pathCollider;
+
+
+    public enum PathMode
+    {
+        Open, Closed
+    };
+    public PathMode pathMode;
+
+    public void Setup()
+    {
+        
+    }
+
+    void Validate()
+    {
+        grid = GetComponent<Grid>();
+        if (grid == null)
+            gameObject.AddComponent<Grid>();
+
+        pathCollider = GetComponent<Collider2D>();
+
+        if (pathMode == PathMode.Open)
+            Debug.Assert(pathCollider is EdgeCollider2D);
+        if (pathMode == PathMode.Closed)
+            Debug.Assert(pathCollider is PolygonCollider2D);
+    }
+
+    void Initialize()
+    {
+        
+    }
+
+    private void Awake()
+    {
+        Initialize();
+    }
+
+    private void Reset()
+    {
+        GetComponent<Collider2D>().isTrigger = true;
+    }
+}
