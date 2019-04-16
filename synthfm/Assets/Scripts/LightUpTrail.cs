38e051592de0e75e183ecad9ed19d183e9e55037
Srija <u1209446@utah.edu>
2019-04-16 02:32:17 -0600

    okay. Something like a preliminary latte script?

--- synthfm/Assets/Scripts/LightUpTrail.cs
+++ synthfm/Assets/Scripts/LightUpTrail.cs
@@ -6,6 +6,10 @@ public class LightUpTrail : MonoBehaviour
 {
     Grid grid;
     Collider2D pathCollider;
+    Vector2[] points;
+    new LineRenderer renderer; //hides base member. We don't care.
+
+    public float segmentSize;
 
 
     public enum PathMode
@@ -16,26 +20,49 @@ public class LightUpTrail : MonoBehaviour
 
     public void Setup()
     {
-        
+        Validate();
+
+        Vector3[] linePoints = new Vector3[points.Length];
+        for (int i = 0; i < points.Length; i++)
+        {
+            linePoints[i].x = points[i].x;
+            linePoints[i].y = points[i].y;
+            linePoints[i].z = transform.position.z;
+        }
+        renderer.positionCount = linePoints.Length;
+        renderer.SetPositions(linePoints);
     }
 
     void Validate()
     {
         grid = GetComponent<Grid>();
         if (grid == null)
-            gameObject.AddComponent<Grid>();
+            grid = gameObject.AddComponent<Grid>();
 
         pathCollider = GetComponent<Collider2D>();
 
+        renderer = GetComponent<LineRenderer>();
+        if (renderer == null)
+            renderer = gameObject.AddComponent<LineRenderer>();
+
         if (pathMode == PathMode.Open)
+        {
             Debug.Assert(pathCollider is EdgeCollider2D);
+            points = ((EdgeCollider2D)pathCollider).points;
+            renderer.loop = false;
+        }
         if (pathMode == PathMode.Closed)
+        {
             Debug.Assert(pathCollider is PolygonCollider2D);
+            points = ((PolygonCollider2D)pathCollider).points;
+            renderer.loop = true;
+        }
+        
     }
 
     void Initialize()
     {
-        
+        Validate();
     }
 
     private void Awake()
@@ -46,5 +73,10 @@ public class LightUpTrail : MonoBehaviour
     private void Reset()
     {
         GetComponent<Collider2D>().isTrigger = true;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+           
     }
 }
