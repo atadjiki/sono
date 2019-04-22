2a0ea86d466a8e9710fd98b8055e4d9d2acbd199
Travis Treadway <Travis.treadway55@gmail.com>
2019-04-22 17:31:05 -0600

    FX

--- synthfm/Assets/TravisCollisionTest.cs
+++ synthfm/Assets/TravisCollisionTest.cs
@@ -4,22 +4,36 @@ using UnityEngine;
 using UnityEngine.Experimental.VFX;
 public class TravisCollisionTest : MonoBehaviour
 {
+    [SerializeField] private VisualEffect vfx;
+
+    public CircleCollider2D cc;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Physics2D.IgnoreLayerCollision(1, 9);
+        Physics2D.IgnoreCollision(GameObject.Find("Player").GetComponent<PolygonCollider2D>(), cc);
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.name == "Player")
+        if (collision.gameObject.name == "Player")
         {
             //TO DO: For Travis - Instantiate your VFX here!
+            vfx.Play();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
         }
     }
 }
