7e1da7549717ad86c88271ca3e5302a4cf1b4159
Arash Tadjiki <atadjiki@yahoo.com>
2019-04-21 18:07:19 -0600

    Update fxtoggle

--- synthfm/Assets/FXToggle.cs
+++ synthfm/Assets/FXToggle.cs
@@ -66,20 +66,15 @@ public class FXToggle : MonoBehaviour
         if (enteredPuzzle)
         {
             LatteFX.SetActive(false);
-            foreach(ParticleSystem p in puzzle.GetComponentsInChildren<ParticleSystem>())
-            {
-                p.Play();
-                Debug.Log("Playing particle system " + p.gameObject.name);
-            }
+            //foreach(ParticleSystem p in puzzle.GetComponentsInChildren<ParticleSystem>())
+            //{
+            //    p.Play();
+            //    Debug.Log("Playing particle system " + p.gameObject.name);
+            //}
         }
         else
         {
             LatteFX.SetActive(true);
-            foreach (ParticleSystem p in puzzle.GetComponentsInChildren<ParticleSystem>())
-            {
-                p.Stop();
-                Debug.Log("Stopping particle system " + p.gameObject.name);
-            }
         }
     }
 
