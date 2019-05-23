496b82bf9692a4f0796fe9b6ef48ac0549782dc5
Srija <u1209446@utah.edu>
2019-05-23 17:54:34 -0600

    Setting up for some good stuff :)

---
+++ synthfm/Assets/Scripts/Audio Management/ManagedSource.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+//[RequireComponent(typeof(AudioSource))]
+public class ManagedSource : MonoBehaviour
+{
+    AudioSource source;
+    ScoreManager scoreManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
